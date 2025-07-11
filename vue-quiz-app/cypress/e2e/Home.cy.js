6ece1df19f465faf068721ff34bba117f3c18f65
Ruhi Ambelkar <121443401+ruhiambelkar17@users.noreply.github.com>
2025-07-11 11:16:20 +0530

    added test cases

--- vue-quiz-app/cypress/e2e/Home.cy.js
+++ vue-quiz-app/cypress/e2e/Home.cy.js
@@ -1,5 +1,9 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('http://localhost:5173') // change URL to match your dev URL
+    cy.visit('/') //to open/visit our application
+    cy.contains('Sign Up') // to check page contains "sign up" text
+    cy.get('input[name="fullname"]').type("abc") //to checther able to type
+    cy.get('input[name="email"]').type("abc@gmail.com");
+    cy.get('input[name="password"]').type("abc@gmail.com");
   })
 })
